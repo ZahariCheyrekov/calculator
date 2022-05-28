5a85560fa8ebae9a82dece9b9a4ecb4a96a6636d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-28 17:40:33 +0300

    Add calculation base logic

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,6 @@
 const numberField = document.querySelector('.number__field__content');
-const buttons = Array.from(document.querySelectorAll('.calculator__buttons li'));
+const buttons = document.querySelectorAll('.calculator__buttons li');
+let invalidExpression = false;
 
 window.addEventListener('DOMContentLoaded', start);
 
@@ -10,5 +11,43 @@ function start() {
 }
 
 function calculate() {
-  
+    const buttonText = this.textContent;
+    const numberFieldText = numberField.textContent;
+
+    if (buttonText == 'C' || buttonText == 'AC' || buttonText == 'MC') {
+        clearNumberField();
+
+    } else if (buttonText == 'DEL') {
+        const text = numberFieldText.toString();
+        numberField.textContent = text.substring(0, text.length - 1);
+
+    } else if (numberFieldText != '' && buttonText == '=') {
+        try {
+            numberField.textContent = eval(numberFieldText);
+        } catch (error) {
+            alert('Invalid Expression');
+            clearNumberField();
+        }
+
+    } else if (numberFieldText == '' && buttonText == '=') {
+        alert('Error!');
+        clearNumberField();
+
+    } else {
+        if (numberFieldText.length == 54) {
+            alert('Error!');
+            clearNumberField();
+
+        } else if (numberFieldText.length < 15) {
+            numberField.classList.remove('smaller__field__size');
+            
+        } else {
+            numberField.classList.add('smaller__field__size');
+        }
+        numberField.textContent += buttonText;
+    }
+}
+
+function clearNumberField() {
+    numberField.textContent = '';
 }
